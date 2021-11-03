fb4d15e0220d86c1b329e5e87cb6652c5d059310
niksvi <77024582+niksvi@users.noreply.github.com>
2021-11-04 00:43:19 +0400

    Навигация, промо, моб.меню

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,33 @@
-'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+const mainHeader = document.querySelector('.main-header');
+const headerToggle = document.querySelector('.site-nav__toggle');
+const mainNav = document.querySelector('.site-nav');
 
-pageHeader.classList.remove('page-header--nojs');
+const form = document.querySelector('form');
+const inputPhone = form.querySelector('input[name=phone]');
+const regularPhone =  /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+
+mainHeader.classList.remove('main-header--nojs');
 
 headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+  if (mainNav.classList.contains('site-nav--closed')) {
+    mainNav.classList.remove('site-nav--closed');
+    mainNav.classList.add('site-nav--opened');
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    mainNav.classList.add('site-nav--closed');
+    mainNav.classList.remove('site-nav--opened');
   }
 });
+
+inputPhone.addEventListener('input', () => {
+  if (!regularPhone.test(inputPhone.value)) {
+    inputPhone.setCustomValidity('Введите корректный номер телефона');
+    inputPhone.style.borderColor = 'red';
+    inputPhone.style.boxShadow = '0 0 10px red';
+
+  } else {
+    inputPhone.setCustomValidity('');
+    inputPhone.style.borderColor = '#c9ccd4';
+    inputPhone.style.boxShadow = '0 0 10px #0ad9c6';
+  }
+  inputPhone.reportValidity();
+})
